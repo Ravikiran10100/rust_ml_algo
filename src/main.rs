9a379bd7ac8cc3d3533106dc9a741e50919b7cd5
imrkweb <ravikiranpasupuleti44@gmail.com>
2026-03-22 00:07:02 -0400

    Testes logic for linear regresion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rust_ml_algo::models::linear_regression::LinearRegression;
+
 fn main() {
-    println!("Hello, world!");
+    let x = vec![vec![1.0, 2.0], vec![2.0, 0.5], vec![3.0, 4.0]];
+    let y = vec![12.0, 9.0, 23.0];
+
+    let mut model = LinearRegression::new(2, 0.01);
+
+    model.train(&x, &y, 1000);
+
+    let preds = model.predict(&x);
+    println!("Predictions: {:?}", preds);
 }
