e40473382a94c39353a14919a519e48231fcf9b3
imrkweb <ravikiranpasupuleti44@gmail.com>
2026-03-22 00:08:07 -0400

    Tested logistic regression logic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use rust_ml_algo::models::linear_regression::LinearRegression;
+use rust_ml_algo::models::logistic_regression::LogisticRegression;
 
 fn main() {
-    let x = vec![vec![1.0, 2.0], vec![2.0, 0.5], vec![3.0, 4.0]];
-    let y = vec![12.0, 9.0, 23.0];
-
-    let mut model = LinearRegression::new(2, 0.01);
-
+    let x = vec![
+        vec![0.0, 0.0],
+        vec![0.0, 1.0],
+        vec![1.0, 0.0],
+        vec![1.0, 1.0],
+    ];
+    let y = vec![0.0, 0.0, 0.0, 0.0];
+    let mut model = LogisticRegression::new(2, 0.1);
     model.train(&x, &y, 1000);
 
     let preds = model.predict(&x);
     println!("Predictions: {:?}", preds);
 }
+
