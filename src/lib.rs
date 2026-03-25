16664edc770bc3fc14a6971f982ed9181f6024ea
imrkweb <ravikiranpasupuleti44@gmail.com>
2026-03-25 18:16:50 -0400

    impl pythob modules in lib.rs file

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
+use pyo3::prelude::*;
+
 pub mod models;
 pub mod python;
+
+use python::python_bindings::PyLinearRegression;
+
+#[pymodule]
+fn rust_ml(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PyLinearRegression>()?;
+    Ok(())
+}
